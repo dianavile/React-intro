7c4a3b43ee9214dc9d206265d5e689363fbe79ac
diana <diana.vile@gmail.com>
2020-11-06 14:31:00 +0100

    update React ToDoApp with MapComponent

--- React-ToDoApp/App.js
+++ React-ToDoApp/App.js
@@ -2,19 +2,18 @@
 import React from "react";
 // Change the input/p combo below to be a new component called <TodoItem />.    
 import TodoItem from "./TodoItem";
+import todosData from "./todosData";
 
 // Have the <App /> component render 3 or 4 checkboxes with paragraphs or spans next to it
 // like you're making a todo list with some hard-coded items on it
 
 //Function App = Table of Contents of Components.
 function App() {
+    const todoItems = todosData.map(item => <TodoItem key={item.id} item={item}/>)
     return (
         //wrap in div, because only can return 1 element at the time.
         <div className="todo-list">
-            <TodoItem />
-            <TodoItem />
-            <TodoItem />
-            <TodoItem />
+             {todoItems}
         </div>
     ) 
 };
--- React-ToDoApp/ToDoItem.js
+++ React-ToDoApp/ToDoItem.js
@@ -7,12 +7,12 @@ import React from "react";
 /*Style up the page however you want! You're welcome to use regular CSS (in the CSS file) or inline styles, or both!*/
 
 //Function App = Table of Contents of Components.
-function ToDoItem() {
+function ToDoItem(props) {
     return (
         //wrap in div, because only can return 1 element at the time.
         <div className="todo-item">
-            <input type="checkbox" />
-            <p>Placeholder text here</p>
+            <input type="checkbox" checked={props.item.completed}/>
+             <p>{props.item.text}</p>
         </div>
     ) 
 };
@@ -22,5 +22,7 @@ export default ToDoItem();
 //apply class to element, you need to used className instead of class.
 //you can apply styles both inline as well as in a apart stylesheet.
 
+
+//turn an Array of data.
  
  
