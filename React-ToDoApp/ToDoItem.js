c8cc15c2217bc0a1df3980ad1edaad0c6b66b04d
diana <diana.vile@gmail.com>
2020-11-06 16:15:30 +0100

    update React ToDoApp with EventHandling

--- React-ToDoApp/ToDoItem.js
+++ React-ToDoApp/ToDoItem.js
@@ -3,15 +3,17 @@ import React from "react";
 
 // Have the <App /> component render 3 or 4 checkboxes with paragraphs or spans next to it
 // like you're making a todo list with some hard-coded items on it
-
-/*Style up the page however you want! You're welcome to use regular CSS (in the CSS file) or inline styles, or both!*/
+/*Style up the page: use regular CSS (in the CSS file) or inline styles, or both!*/
+/*Challenge: Get rid of our warning about not having an onChange on our input. For now, the function that runs onChange can simply console.log something.*/
 
 //Function App = Table of Contents of Components.
 function ToDoItem(props) {
     return (
         //wrap in div, because only can return 1 element at the time.
         <div className="todo-item">
-            <input type="checkbox" checked={props.item.completed}/>
+            <input type="checkbox" checked={props.item.completed}
+                onChange={() => console.log("Changed!")}
+            />
              <p>{props.item.text}</p>
         </div>
     ) 
