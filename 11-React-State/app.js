07c559f5a9c39c179320df78a77d7d31993dc181
diana <diana.vile@gmail.com>
2020-11-06 15:36:58 +0100

    React State + ex

---
+++ 11-React-State/app.js
@@ -0,0 +1,33 @@
+import React from "react";
+
+//A state = data that a component maintains and can change its value.
+//To add state in a component:
+//1) the component needs to be CLASS - BASED!
+//2) and you need to add the constructor()method.It initializes parts of this class.
+//3) and add the super()method, this goes along with the constructor()method.
+//4) add property to this.state = object, to access this state anywhere else.
+//props (the other way to pass data, can NOT be changed, props are inmutable!)
+//this.props.something = "some new value" = NOT OKE!
+//more info about classes: // https://scrimba.com/p/p4Mrt9/cQnMDHD
+
+class App extends React.Component {
+    constructor() {
+        super()
+        //initial value, can always be changed
+        //the state can be passed down to a child components through props.
+        this.state = {
+            answer: "Yes"
+        };
+
+     }
+    render() {
+        return (
+            <div>
+                <h1>Is state important to know? {this.state.answer}</h1>
+                <ChildComponent answer={this.state.answer}/>
+            </div>
+        )
+    }
+}
+
+export default App;
