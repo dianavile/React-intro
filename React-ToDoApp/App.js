d503dbd2123a56cff68d6bf7d23cb27e0cdc7bb6
diana <diana.vile@gmail.com>
2020-11-06 15:57:14 +0100

    add state to React ToDoApp

--- React-ToDoApp/App.js
+++ React-ToDoApp/App.js
@@ -7,15 +7,25 @@ import todosData from "./todosData";
 // Have the <App /> component render 3 or 4 checkboxes with paragraphs or spans next to it
 // like you're making a todo list with some hard-coded items on it
 
+//Challenge: Change the <App /> component into a stateful class component and load the imported `todosData` into state.
 //Function App = Table of Contents of Components.
-function App() {
-    const todoItems = todosData.map(item => <TodoItem key={item.id} item={item}/>)
-    return (
+
+class App extends React.Component {
+    constructor() { 
+        super()
+        this.state = {
+            todos: todosData;
+        }   
+    }
+    render() {
+        const todoItems = this.state.todosData.map(item => <TodoItem key={item.id} item={item}/>)
+        return (
         //wrap in div, because only can return 1 element at the time.
         <div className="todo-list">
              {todoItems}
         </div>
-    ) 
+        ) 
+    }
 };
 
 export default App();
