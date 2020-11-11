cabf85a6e3b0623ea1fe9f108ef949a9a7c0ab51
diana <diana.vile@gmail.com>
2020-11-11 15:00:36 +0100

    setup React Form2

---
+++ 17-React-Forms1/App.js
@@ -0,0 +1,69 @@
+import React, { Component } from "react";
+
+class App extends Component {
+    constructor() {
+        super()
+        this.state = {
+            firstName: "",
+            lastName: ""
+        }
+        this.handleChange = this.handleChange.bind(this);
+    }
+
+    //grab current value
+    //update state to reflect current value
+    handleChange(event) {
+        const { name, value } = event.target;//react syntatic event.
+        this.setState({
+            //on eventlistener get value of specific element
+            [name]: value
+        })
+    }
+    
+    render() {
+        return (
+            <form>
+                {/* //we call a function on Change */}
+                <input
+                    type="text"
+                    value={this.state.firstName}
+                    name="firstName"
+                    placeholder="First Name"
+                    onChange={this.handleChange}
+                />
+                <br />
+                <input
+                    type="text"
+                    value={this.state.lastName}
+                    name="lastName"
+                    placeholder="Last Name"
+                    onChange={this.handleChange}
+                />
+
+                {
+                    /**
+                     * Other useful form elements:
+                     * 
+                     *  <textarea /> element
+                     *  <input type="checkbox" />
+                     *  <input type="radio" />
+                     *  <select> and <option> elements
+                     */
+                }
+                
+                <h1>{this.state.firstName} {this.state.lastName}</h1>
+            </form>
+
+        )
+    }
+}
+
+export default App;
+
+//In React, Forms are constantly updated via State, from the beginning.
+//We want to watch every single update on the form.
+//whereas in Vanilla JS, it is updated and validated at the end.
+
+//controlled form
+//what displayed in form, need to match with state.
+//State should be the single source of truth!
