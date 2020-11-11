47952846850cb73064c20cf5dfbf97e4396397bb
diana <diana.vile@gmail.com>
2020-11-11 08:39:21 +0100

    fix error

--- React-ToDoApp/App.js
+++ React-ToDoApp/App.js
@@ -14,9 +14,7 @@ import todosData from "./todosData";
  * 1. Create an event handler in the App component for when the checkbox is clicked (which is an `onChange` event)
  * a. This method will be the trickest part. Check the comments in the stubbed-out method below for some pseudocode to help guide you through this part
  * 2. Pass the method down to the TodoItem component
- * 3. In the TodoItem component, make it so when the `onChange` event happens, it calls the `handleChange` method and passes the id of the todo into the function
- */
-
+ * 3. In the TodoItem component, make it so when the `onChange` event happens, it calls the `handleChange` method and passes the id */
 class App extends React.Component {
     constructor() { 
         super()
@@ -61,4 +59,4 @@ class App extends React.Component {
     }
 };
 
-export default App();
+export default App;
