43506823cd139829c60924261bdab1508da46fbc
diana <diana.vile@gmail.com>
2020-11-12 13:54:30 +0100

    add setup React Meme Generator

---
+++ React-MemeGenerator/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+/**
+ * Create 2 new components - Header and MemeGenerator
+ * Header will only display things
+ * MemeGenerator will be calling to an API and holding on to data
+ * Each should be in their own file of the same name
+ */
+
+function App() { 
+    return (
+     <h1>Hello World!</h1>
+    )   
+}
+
+export default App;
