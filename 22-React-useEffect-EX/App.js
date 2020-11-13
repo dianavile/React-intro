1b1357b6e5674f9b4ae42a9696116ea93dda3e4c
diana <diana.vile@gmail.com>
2020-11-13 10:26:08 +0100

    update useEffect

--- 22-React-useEffect-EX/App.js
+++ 22-React-useEffect-EX/App.js
@@ -1,48 +1,43 @@
-import React, {useState} from "react";
+import React, { useState, useEffect } from "react";
+import randomcolor from "randomcolor";
 
-//How to change the state using Hooks
-// Convert the class below to a functional component that uses the useState hook to initalize a count vartiable to 0 and display the count on the screen.
-// Don't worry about the part where the button changes the count quite yet, that's what you're here to learn about!
-
-function App() { 
+function App() {
     const [count, setCount] = useState(0);
-    //const [answer, setAnswer] = useState("Yes")
-    function increment() { 
-        setCount(prevCount => prevCount + 1)
-    }
-    function decrement() { 
-        setCount(prevCount => prevCount - 1)
-    }
-    //console.log(count);
+    const [color, setColor] = useState("");
+
+    //we can call useEffect() more then once.
+    //something specific to only run once, use empty array
+    useEffect(() => {
+        const intervalID = setInterval(() => {
+            setCount(prevCount => prevCount + 1)
+        }, 1000)
+         //cleanup
+        return () => {clearInterval(intervalId);}
+    }, []);
+    
+    useEffect(() => {
+        setColor(randomcolor())
+    }, [count]);
+
     return (
         <div>
-            <h1>{count}</h1>
-            <button onClick={increment}>Increment!</button>
-            <button onClick={decrement}>Decrement!</button>
+            <h1 style={{color: color}}>{count}</h1>
         </div>
     )
 }
-// class App extends React.Component {
-//     constructor() {
-//         super()
-//         this.state = {
-//             count: 0,
-//             answer: "Yes"
-//         }
-//     }
-    
-//     render() {
-//         return (
-//             <div>
-//                 <h1>{this.state.count}</h1>
-//                 <button>Change!</button>
-//             </div>
-//         )
-//     }
-// }
 
 export default App;
 
-//To save different parts of state that are not really connected,
-//instead of mashing all together into an {object}, 
-//create multiple versions of state.
+
+//until now, logic with component,
+//yet some sideEffect will not clean up automatically
+// 1) create a manual eventListener() for a part of your document,
+ //document.addEventListener(...)//does NOT clean up automatically
+// 2) socket subscription= watches for changes in realtime (ex: realtime chat)
+// Use setInterval() , native build-in JS function, componentWillUnmount, to clean up automatically.
+
+
+//useEffect, introducing sideEffects into a component.
+//1) replace componentDidMount- [] with empty array.
+//2) replace componentDidUpdate- add variables to [] array.
+//3) replace componentWillUnmount- return function for cleanup.
